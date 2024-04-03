bb7798ade83168cbe87e91f08227697e870bd881
phuctnh <kingherotd@gmail.com>
2024-04-03 10:20:41 +0700

    updated 3/4/24

--- src/main/java/phuctnh/example/osrm_project/Controller.java
+++ src/main/java/phuctnh/example/osrm_project/Controller.java
@@ -9,12 +9,20 @@ import java.util.ArrayList;
 @RestController
 public class Controller {
     private final Analyist analyist = new Analyist();
-    @GetMapping("/search")
-    public String getRouteInfo(@RequestParam String place1, @RequestParam String place2, @RequestParam String transportation)
+    @GetMapping("/search/route")
+    public String getRouteInfo(@RequestParam String startingpoint, @RequestParam String destination, @RequestParam String transportation)
     {
-        ArrayList<Double> start = analyist.getPlace(place1);
-        ArrayList<Double> end = analyist.getPlace(place2);
+        ArrayList<Double> start = analyist.getPlace(startingpoint);
+        ArrayList<Double> end = analyist.getPlace(destination);
         String routeInfo = analyist.getRoute(start.get(0), start.get(1), end.get(0), end.get(1), transportation);
         return routeInfo;
+    }
+    @GetMapping("/search")
+    public String getLocation(@RequestParam String startingpoint, @RequestParam String destination, @RequestParam String transportation)
+    {
+        ArrayList<Double> startLocation = analyist.getPlace(startingpoint);
+        ArrayList<Double> endLocation = analyist.getPlace(destination);
+        String res = "Starting point: ["+ startLocation.get(0) +", " + startLocation.get(1) + "]; Destination: [" + endLocation.get(0) +", " + endLocation.get(1)+"]";
+        return res;
     }
 }
